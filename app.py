f4848cf65f881220f9b1b5b12f90fad58c5ccd3f
Jack Kenning <jack.j.kenning@gmail.com>
2021-02-21 16:30:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -34,4 +34,15 @@ def welcome():
         /api/v1.0/tobs
         /api/v1.0/temp/start/end
         ''')
+# create precipitation analysis route
+@app.route("/api/v1.0/precipitation")
+# create precipitation function
+def precipitation ():
+    prev_year = dt.date(2017,8,23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
+
+
 
